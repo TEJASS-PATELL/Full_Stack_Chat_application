668de285ed9ca989f7de49d810ece58262f425d8
Tejas <tejasspatell2@gmail.com>
2025-09-04 01:57:28 +0530

    fix host chat app

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,12 +2,12 @@ const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = mysql.createPool({
+const aiven_config = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT,
+  port: process.env.PORT, 
   ssl: {
     ca: process.env.CA_CERT.replace(/\\n/g, '\n'),
     rejectUnauthorized: true,
@@ -15,8 +15,19 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
+};
 
-console.log("MySQL Pool Created Successfully");
+async function connectToDatabase() {
+  try {
+    const pool = mysql.createPool(aiven_config);
+    console.log("MySQL Pool Created Successfully");
+    return pool;
+  } catch (error) {
+    console.error("Error creating MySQL pool:", error);
+    process.exit(1);
+  }
+}
+
+const pool = connectToDatabase();
 
 module.exports = pool;
