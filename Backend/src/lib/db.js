47c3af4a629dcb150eb74e34911e4e32b5edfec3
Tejas <tejasspatell2@gmail.com>
2025-09-04 01:39:57 +0530

    fix host chat app

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.DB_PORT,
+  port: process.env.PORT,
   ssl: {
     ca: process.env.CA_CERT.replace(/\\n/g, '\n'),
     rejectUnauthorized: true,
