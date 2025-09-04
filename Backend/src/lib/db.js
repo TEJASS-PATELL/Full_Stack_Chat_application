7e25e6f55a7ebc8f93dd7b99df1046821e57cc52
Tejas <tejasspatell2@gmail.com>
2025-09-04 17:51:39 +0530

    fix host chat app

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -3,9 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.INTERNAL_URL, 
-  ssl: {
-    rejectUnauthorized: false, 
-  },
+  ssl: false,
   max: 10,               
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
