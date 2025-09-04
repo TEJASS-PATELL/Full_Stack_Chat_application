3e9f7c720cc8b32b7096c2c99c8e7e239be60f1c
Tejas <tejasspatell2@gmail.com>
2025-09-04 15:27:06 +0530

    fix host chat app

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,17 +2,13 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
-    ssl: {
+  connectionString: process.env.INTERNAL_URL, 
+  ssl: {
     rejectUnauthorized: false, 
   },
-  max: 10,                
+  max: 10,               
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000, 
+  connectionTimeoutMillis: 2000,
 });
 
 (async () => {
