8c891ed4dfce21661baca941aea342f37328d586
Tejas <tejasspatell2@gmail.com>
2026-01-16 16:08:48 +0530

    fix some error api error

--- Frontend/src/Pages/LoginPage.jsx
+++ Frontend/src/Pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import { useAuthStore } from "../Store/useAuthStore";
 import { Link } from "react-router-dom";
 import { FaLock, FaLockOpen } from 'react-icons/fa';
